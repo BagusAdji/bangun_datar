369edd5aa022ce307e992fe4fcbc067e315746bf
KarylTzy <bgsadji@gmail.com>
2023-11-14 14:41:45 +0700

    initial commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,22 +15,38 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-              color: Colors.blue
-            ),
-            child: Text("Persegi"),
+          CustomMenu(imageAssets: "assets/persegi.png",title: "persegi",),
+          CustomMenu(imageAssets: "assets/segitiga.png",title: "segitiga",),
+          CustomMenu(imageAssets: "assets/lingkaran.png",title: "lingkaran",)
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.blue
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+              imageAssets,
+              height: 100),
+          Text(
+            title
           ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.blue
-            ),
-            child: Text("Segitiga"),
-          )
         ],
       ),
     );
